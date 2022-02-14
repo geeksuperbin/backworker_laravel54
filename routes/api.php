3f0cddd644f2550ab0de36a33425e294ae97b867
geeksuperbin <liubindashen@gmail.com>
2022-02-14 17:40:58 +0800

    添加 路由注释

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,9 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
+| 您可以在此处为您的应用程序注册 API 路由。 这些
+| 路由由 RouteServiceProvider 在一个组内加载
+| 被分配了“api”中间件组。 享受构建您的 API 的乐趣！
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+
+| 您可以在此处为您的应用程序注册网络路由。 这些
+| 路由由 RouteServiceProvider 在一个组内加载
+| 包含“web”中间件组。 现在创造一些伟大的东西！
 */
 
 Route::get('/', function () {
